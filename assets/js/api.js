984ab90c21aa1aaf5786922fb5ef1f4b7cd26939
João Minikoski <jlucasminikoski111@gmail.com>
2025-08-09 12:51:06 -0300

    Implementação da requisição de detalhes, simplificação de algumas partes do código e integração dos detalhes ao HTML. Próximo passo será editar o CSS.

---
+++ assets/js/api.js
@@ -0,0 +1,43 @@
+const pokeApi = {}
+
+function convertApiPokeToModelPoke(detalhesPokemon){
+    const pokemon = new Pokemon()
+    pokemon.nome = detalhesPokemon.name
+    pokemon.numero = detalhesPokemon.id
+
+
+    const tipos = detalhesPokemon.types.map((typeSlot) => typeSlot.type.name)
+    const [tipo] = tipos
+    
+    pokemon.tipos = tipos
+    pokemon.tipoMain = tipo
+
+    pokemon.imagem = detalhesPokemon.sprites.other.dream_world.front_default
+
+    return pokemon
+}
+
+pokeApi.getPokemon = (offset = 0, limit = 10) => {
+
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonbody) => jsonbody.results)
+        .then
+        (
+            (pokemons) => {
+                return pokemons.map(
+                    (pokemon) => fetch(pokemon.url)
+                        .then((response) => response.json())
+                        .then(convertApiPokeToModelPoke)
+                )
+            }
+        )
+        .then(
+        (detalhesRequests) => Promise.all(detalhesRequests)
+        )
+        .then(
+            (detalhesPokemon) => detalhesPokemon)
+}
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,33 @@
-
-//Etapa Transformando nossa lista de Pokémon em uma lista de HTML
-
-const limit = 15;
-const offset = 0;
-const url = "https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}";
+function htmlTipos(pokemonTypes) {
+    return pokemonTypes.map((typeName) => `<li class="type">${typeName}</li>`)
+}
 
 
 function htmlPokemon(pokemon) {
     return `<li class="pokemon">
-                <span class="number">#001</span>
-                <span class="name">${pokemon.name}</span>
+                <span class="number">${pokemon.numero}</span>
+                <span class="name">${pokemon.nome}</span>
 
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
-                        <li class="type">poison</li>
+                        ${htmlTipos(pokemon.tipos).join('')}                        
                     </ol>
 
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
-                        alt="${pokemon.name}">
+                    <img src="${pokemon.imagem}"
+                        alt="${pokemon.nome}">
                 </div>
             </li>`
 }
 
-
 const htmlPokemonList = document.getElementById("pokemonList");
-
-
 //Versão Simplificada com Arrow Function
-fetch(url)
-.then ((response) => response.json())
-.then((jsonbody) => jsonbody.results)
-.then((pokemonList) => {
-    for (let i = 0; i < pokemonList.length; i++) {
-        const pokemon = pokemonList[i];
-        htmlPokemonList.innerHTML += htmlPokemon(pokemon);
-    }
+pokeApi.getPokemon().then((pokemonList = []) => {
+    //Versão super resumida do map
+    const newHtml = pokemonList.map(htmlPokemon).join('')
+    htmlPokemonList.innerHTML += newHtml
 })
-.catch( (error)=> console.log(error))
-.finally(() => console.log("Requisição concluída"))
+    .catch((error) => console.log(error))
+    .finally(() => console.log("Requisição concluída"))
 
 
 
@@ -46,6 +35,29 @@ fetch(url)
 
 
 
+    
+    // //Versão simplificada com MAP convertendo a lista de pokemon na lista de Li's do HTML
+    // const listaConvert = pokemonList.map((pokemon) => {
+    //     return htmlPokemon(pokemon)
+    // });    
+
+    // //Unindo os itens da lista
+    // const newHtml = listaConvert.join('')
+    
+    // htmlPokemonList.innerHTML += newHtml
+
+
+
+
+
+
+    // // Versão sem MAP
+    // const listItem = []
+    // for (let i = 0; i < pokemonList.length; i++) {
+    //     const pokemon = pokemonList[i];
+    //     listItem.push(htmlPokemon(pokemon))
+    // }
+    // console.log(listItem)
 
 
 
